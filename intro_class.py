ce4b5f4283c547f1ae042f32e7934385f979c5d3
sydney <sydneykiki40@gmail.com>
2025-05-15 17:23:26 +0300

    first commit

---
+++ intro_class.py
@@ -0,0 +1,19 @@
+print("sydney kinya")
+print("100")
+print("kiki")
+
+x=20
+y=5
+z=x+y
+print(z)
+my_student='alex'
+print(my_student)
+#calculate area of a square
+side=20
+area=side*side
+print(area)
+#calculate area of a rectangle
+length=20
+width=15
+area=length*width
+print(area)
